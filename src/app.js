62b771e1ee8768626bff1aa7bf1701bc781b7226
ankitscode <ankitsinghhp97@gmail.com>
2025-09-23 16:23:25 +0530

    initalized project

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/database.js'; 
+import routes from './routes/index.js';
+
+dotenv.config({quiet: true});
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+// 404 handler
+app.use((req, res, next) => res.status(404).json({ status: 404, message: 'Not found' }));
+
+// default error handler
+app.use((err, req, res, next) => res.status(500).json({ status: 500, message: 'Oops, something went wrong' }));
+
+export default app;
